db905a8888a86285a4f000a6ec31cd94b7116e91
asmaasaleh185 <asmaasaleh185@gmail.com>
2025-10-11 00:16:08 +0300

    feat: Implement Notification model with relationships and methods

---
+++ app/Models/Notification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Notification extends Model
+{
+    /** @use HasFactory<\Database\Factories\NotificationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'message',
+        'type',
+        'is_read'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->update(['is_read' => true]);
+        return $this;
+    }
+
+    public function isRead()
+    {
+        return (bool) $this->is_read;
+    }
+}
